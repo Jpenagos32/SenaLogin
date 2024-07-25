de0ee6da3fe4b8b68a64c9223a194bd2dd3c2d1b
jpenagos32 <jpenagosdev@gmail.com>
2024-07-25 00:16:17 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  const {user, password} = req.query
+  const loginData ={
+    usr: 'jpenagos',
+    pass: 'abc123'
+  }
+
+  if(user === loginData.usr && password === loginData.pass){
+    return res.render('logged', { title: 'Usted Ha Iniciado Sesión', user });
+  }
+  if(!user || !password){
+    return res.render('notLogged', {title: 'Error', message: 'Debe proporcionar usuario y contraseña'})
+  }
+  return res.render('notLogged', {title: 'Error', message: 'Usuario o contraseña incorrectos'})
+});
+
+module.exports = router;
